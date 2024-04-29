1d9f8e87dd675f72a35502182ee6ca7debf02c8d
fedor705804@gmail.com <fedor705804@gmail.com>
2024-04-29 11:58:29 +0300

    часы

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from turtle import *
-n = int(input("Сколько сторон у многоугольника? "))
-for i in range(n):
- forward(100)
- left(360//n)
+import turtle
+
+a = turtle.Screen()
+t = turtle.Turtle()
+for i in range(8):
+ t.forward(100)
+ t.left(45)
+for i in range(8):
+ t.forward(50)
+ t.left(90)
+ t.forward(20)
+ t.left(180)
+ t.forward(20)
+ t.left(90)
+ t.forward(50)
+ t.left(45)
+input("Нажми, чтоб закрыть программу")
